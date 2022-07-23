fe336b392472d1b76af114b8957e7629b1355ae8
Jonathan Reis <jonathanreisdeandrade@gmail.com>
2022-07-23 01:12:52 -0300

    configura o cors

--- src/api/app.js
+++ src/api/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const rescue = require('express-rescue');
+const cors = require('cors');
 const UserController = require('../controllers/user');
 const errorMiddleware = require('../middlewares/error');
 
 const app = express();
+
+const corsOptions = {
+  origin: 'https://heroku-frontend-123.herokuapp.com',
+};
+
+app.use(cors(corsOptions));
 
 app.get('/users', rescue(UserController));
 
